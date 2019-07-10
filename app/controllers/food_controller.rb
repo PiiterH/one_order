69e5342b500d245b370a45d1c9ca171acc2d77e7
Peter Hsieh <chunhung.hsieh@gmail.com>
2019-07-10 11:44:35 +0800

    finished

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,2 +1,25 @@
 class FoodController < ApplicationController
+
+
+def index
+  @foods = Food.all
+end
+
+def show
+  @food = Food.find(params[:id])
+end
+
+def max_foods
+  @max_foods = []
+  @max_foods << @food.meal
+  @max_foods.count
+end
+
+
+private
+
+def food_params
+  params.require(:food).permit(:name)
+end
+
 end
--- app/controllers/meal_controller.rb
+++ app/controllers/meal_controller.rb
@@ -1,2 +1,23 @@
 class MealController < ApplicationController
+def index
+  @meals = Meal.all
+end
+
+def @meal = Meal.find(params[:id])
+
+def no_food_meal
+  @no_food_meal = @meals.foods.name.empty?
+end
+
+def other_food
+  @food_head = @food
+  @other_food = @meal.foods.name - @food_head
+end
+
+private
+
+def meal_params
+  params.require(:meal).permit(:name)
+end
+
 end
